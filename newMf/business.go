da85d9d6782df89c3637ccf95e23d18765acbde6
huoxuhuoxu <530607199@qq.com>
2019-11-01 01:41:27 +0800

    update

---
+++ newMf/business.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+// ------------------------------------ 获取实时基础数据
+type GeneralData struct {
+	stockType      string  // 上市类型
+	stockCode      string  // 代码
+	stockName      string  // 名称
+	currentPrice   float64 // 当前价格
+	upAndDownRange float64 // 涨跌幅度
+	turnover       float64 // 成交额
+	high           float64 // 盘中最低
+	low            float64 // 盘中最高
+	open           float64 // 开盘价
+	close          float64 // 前个交易日收盘价
+	min5           float64 // 5分钟涨速
+	changehands    float64 // 换手率
+	circulation    float64 // 流通市值
+
+	turnoverToCirculation float64 // 成交额占流通市值比例
+
+	// 所有的流入与流出其实只是一个资金意向问题, 一笔交易有钱出去就意味着有钱进来, 盈亏只是这笔交易关联的上下文的价格的差价
+	mainV   float64 // 主力净流入(由超大单与大单组成)
+	cBigV   float64 // 超大单净流入
+	bigV    float64 // 大单净流入
+	middleV float64 // 中单净流入
+	smallV  float64 // 小单净流入
+
+	mainP float64 // 主力流入占比
+}
+
+func GetDatas() map[string]*GeneralData {
+	gds := make(map[string]*GeneralData)
+
+	// 获取原始数据
+	initUrl := "http://nufm.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx?cb=jQuery112407378636087109598_1528454080519&type=CT&token=4f1862fc3b5e77c150a2b985b12db0fd&js=(%7Bdata%3A%5B(x)%5D%2CrecordsTotal%3A(tot)%2CrecordsFiltered%3A(tot)%7D)&cmd=C._A&sty=FCOIATC&st=(ChangePercent)&sr=-1&p=1&ps=10000&_=1528454080520"
+	header := make(map[string]string)
+	header["Accept"] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+	header["Accept-Encoding"] = "gzip, deflate"
+	header["Accept-Language"] = "zh-CN,zh;q=0.9,en;q=0.8"
+	header["Cache-Control"] = "n-cache"
+	header["Connection"] = "keep-alive"
+	header["Host"] = "nufm.dfcfw.com"
+	header["Pragma"] = "no-cache"
+	header["Upgrade-Insecure-Requests"] = "1"
+	header["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", initUrl, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for key, v := range header {
+		req.Header.Add(key, v)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 读取数据
+	reader, err := gzip.NewReader(bytes.NewBuffer(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+	tmpBytes, err := ioutil.ReadAll(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 解析数据
+	str := string(tmpBytes)
+	ret := strings.Split(strings.Split(str, "data:[\"")[1], "\"],")
+
+	strData := ret[0]
+	datas := strings.Split(strData, "\",\"")
+
+	for _, data := range datas {
+		arr := strings.Split(data, ",")
+
+		// 排除长创业版和科创板
+		if strings.HasPrefix(arr[1], "300") || strings.HasPrefix(arr[1], "688") {
+			continue
+		}
+		// 排除停牌的和ST的
+		if arr[4] == "-" || strings.HasPrefix(arr[2], "*ST") || strings.HasPrefix(arr[2], "ST") {
+			continue
+		}
+
+		currentPrice, _ := strconv.ParseFloat(arr[3], 64)
+		upAndDownRange, _ := strconv.ParseFloat(arr[5], 64)
+		turnover, _ := strconv.ParseFloat(arr[7], 64)
+		high, _ := strconv.ParseFloat(arr[9], 64)
+		low, _ := strconv.ParseFloat(arr[10], 64)
+		open, _ := strconv.ParseFloat(arr[11], 64)
+		close, _ := strconv.ParseFloat(arr[12], 64)
+		min5, _ := strconv.ParseFloat(arr[13], 64)
+		changehands, _ := strconv.ParseFloat(arr[15], 64)
+		circulation, _ := strconv.ParseFloat(arr[19], 64)
+
+		gds[arr[1]] = &GeneralData{
+			stockType:             arr[0],
+			stockCode:             arr[1],
+			stockName:             arr[2],
+			currentPrice:          currentPrice,
+			upAndDownRange:        upAndDownRange,
+			turnover:              turnover,
+			high:                  high,
+			low:                   low,
+			open:                  open,
+			close:                 close,
+			min5:                  min5,
+			changehands:           changehands,
+			circulation:           circulation,
+			turnoverToCirculation: turnover / circulation,
+		}
+	}
+
+	return gds
+}
+
+// ------------------------------------- 获取实时资金数据
+func GetMoneyData() map[string]*GeneralData {
+	gds := GetDatas()
+
+	// 获取原始数据
+	header := make(map[string]string)
+	header["Accept"] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+	header["Accept-Encoding"] = "gzip, deflate"
+	header["Accept-Language"] = "zh-CN,zh;q=0.9,en;q=0.8"
+	header["Cache-Control"] = "n-cache"
+	header["Connection"] = "keep-alive"
+	header["Host"] = "nufm.dfcfw.com"
+	header["Pragma"] = "no-cache"
+	header["Upgrade-Insecure-Requests"] = "1"
+	header["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+
+	client := &http.Client{}
+	// 只抓取主力资金流入的前3000支票
+	req, err := http.NewRequest("GET", "http://push2.eastmoney.com/api/qt/clist/get?pn=1&pz=3000&po=1&np=1&ut=b2884a393a59ad64002292a3e90d46a5&fltt=2&invt=2&fid0=f4001&fid=f62&fs=m:0+t:6+f:!2,m:0+t:13+f:!2,m:0+t:80+f:!2,m:1+t:2+f:!2,m:1+t:23+f:!2,m:0+t:7+f:!2,m:1+t:3+f:!2&stat=1&fields=f12,f14,f2,f3,f62,f184,f66,f69,f72,f75,f78,f81,f84,f87,f204,f205,f124&rt=52415130&cb=jQuery183019370468044060885_1572453915798&_=1572453916366", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for key, v := range header {
+		req.Header.Add(key, v)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader, err := gzip.NewReader(bytes.NewBuffer(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+	tmpBytes, err := ioutil.ReadAll(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 读取数据
+	rawDataStr := string(tmpBytes)
+	originDataStr := strings.Split(strings.Split(rawDataStr, "(")[1], ")")[0]
+
+	// 解析数据
+	var data = make(map[string]interface{})
+	err = json.Unmarshal([]byte(originDataStr), &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmpData := data["data"].(map[string]interface{})
+	list := tmpData["diff"].([]interface{})
+
+	// 重组数据
+	for _, v := range list {
+		g := v.(map[string]interface{})
+		if gd, ok := gds[g["f12"].(string)]; ok {
+			gd.mainV = g["f62"].(float64)
+			gd.cBigV = g["f66"].(float64)
+			gd.bigV = g["f72"].(float64)
+			gd.middleV = g["f78"].(float64)
+			gd.smallV = g["f84"].(float64)
+			gd.mainP = g["f69"].(float64)
+		}
+	}
+
+	// 删除无需使用的股
+	for k, gd := range gds {
+		if gd.mainV == 0 {
+			delete(gds, k)
+		}
+	}
+
+	return gds
+}
---
+++ newMf/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+var (
+	minTurnover              float64 = 2    // 最少成交额 2 亿
+	minChangehands           float64 = 2    // 最低换手 2%
+	minTurnoverToCirculation float64 = 0.02 // 成交占比流通最少 万2
+	minMainP                 float64 = 5    // 主力流入占比最低 5%
+	mainMainV                float64 = 0.1  // 主力流入最少 0.1 亿
+)
+
+func init() {
+	flag.Float64Var(&minTurnover, "mt", 2, "最少成交额")
+	flag.Float64Var(&minChangehands, "mc", 2, "最低换手")
+	flag.Float64Var(&minTurnoverToCirculation, "mttc", 0.02, "成交占比流通")
+	flag.Float64Var(&minMainP, "mp", 5, "主力流入占比")
+	flag.Float64Var(&mainMainV, "mv", 0.1, "主力流入")
+	flag.Parse()
+}
+
+func main() {
+	gds := GetMoneyData()
+	log.Println(len(gds))
+
+	showGds := make([]*GeneralData, 0)
+	for _, gd := range gds {
+		gd.turnover = gd.turnover / 100000000
+		gd.mainV = gd.mainV / 100000000
+
+		if gd.mainV < mainMainV {
+			continue
+		}
+		if gd.mainP < minMainP {
+			continue
+		}
+		if gd.turnoverToCirculation < minTurnoverToCirculation {
+			continue
+		}
+		if gd.changehands < minChangehands {
+			continue
+		}
+		if gd.turnover < minTurnover {
+			continue
+		}
+		showGds = append(showGds, gd)
+	}
+
+	fmt.Printf("%-6s, %-4s, %-4s, %-4s, %-4s, %-4s, %-4s, %-4s \r\n",
+		"代码", "名称", "涨跌幅", "成/流", "主力流入", "占比", "5min", "换手")
+
+	var (
+		total, fCount, zCount float64
+	)
+	for _, gd := range showGds {
+		fmt.Printf("%6s, %4s, %6.2f%%, %6.4f%%, %6.2f亿, %6.2f%%, %6.2f%%, %2.2f%% \r\n",
+			gd.stockCode,
+			gd.stockName,
+			gd.upAndDownRange,
+			gd.turnoverToCirculation,
+			gd.mainV,
+			gd.mainP,
+			gd.min5,
+			gd.changehands,
+		)
+
+		total++
+		if gd.upAndDownRange > 0 {
+			zCount++
+		} else {
+			fCount++
+		}
+	}
+
+	fmt.Printf("\r\n涨幅胜率: %.2f%%\r\n", zCount/total*100)
+	fmt.Printf("跌幅胜率: %.2f%%", fCount/total*100)
+}
