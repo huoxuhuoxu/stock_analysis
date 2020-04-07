85b574165c0ed899bda2184ca2e81e59f26947ff
huoxuhuoxu <530607199@qq.com>
2020-04-07 20:10:14 +0800

    update

--- show/main.go
+++ show/main.go
@@ -52,11 +52,11 @@ func show() {
 	sort.Sort(keys)
 	switch mode {
 	case 0:
-		keys = sort.StringSlice{"a2009", "m2101", "c2009", "I2009", "CF009", "AU2006"}
+		keys = sort.StringSlice{"a2009", "m2101", "c2009", "Y2009", "JD2009", "CF009"}
 	case 1:
 		keys = sort.StringSlice{"a2009", "m2101", "c2009"}
 	case 2:
-		keys = sort.StringSlice{"a2009", "m2101", "c2009", "Y2009", "JD2009", "rb2101"}
+		keys = sort.StringSlice{"a2009", "m2101", "c2009", "Y2009", "JD2009"}
 	}
 
 	for {
