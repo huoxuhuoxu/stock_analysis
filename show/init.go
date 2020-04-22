eee4f14618a346b5eb74a24178827cef65456dbe
huoxuhuoxu <530607199@qq.com>
2020-04-22 23:04:15 +0800

    update

--- show/init.go
+++ show/init.go
@@ -65,53 +65,59 @@ var varietys = map[string]*Variety{
 	"rb2101": &Variety{
 		Name:          "螺纹",
 		OriginDataUrl: "113_rb2101_qt?callbackName=aa&cb=aa&_=1587308954178",
-		SpotPrice:     3431,
+		SpotPrice:     3405.5,
 		Aims:          "3400",
 		Describe: `
 			供需强劲, 关联原料铁矿石也很强劲, 进入平台区, 一旦突破, 3400指日可待
 			反之, 3100也很容易回踩, 3250为中线, 上下各150点宽幅震荡
 		`,
-		Level:          1,
+		Level:          2,
 		PricePrecision: 0,
 		IsShow:         true,
 	},
 	"i2009": &Variety{
 		Name:          "铁矿石",
 		OriginDataUrl: "114_i2009_qt?callbackName=aa&cb=aa&_=1587396948222",
-		SpotPrice:     670,
-		Aims:          "620",
+		SpotPrice:     663.89,
+		Aims:          "640",
 		Describe: `
 			减产, 但是四五月份到港量高压制价格继续上行
-			技术面, 昂扬向上, ... 
-			暂不做单边观点
+			可以与螺纹联做, 日内 多铁矿空螺纹
+			目前 铁矿 强于 螺纹, 1:2 或 2:3 建仓, 不跨日持仓
 		`,
-		Level:          4,
+		Level:          2,
 		PricePrecision: 1,
 		IsShow:         true,
 	},
 	"m2101": &Variety{
 		Name:          "豆粕",
 		OriginDataUrl: "114_m2101_qt?callbackName=aa&cb=aa&_=1585752611719",
-		SpotPrice:     3160,
+		SpotPrice:     3116.25,
 		Aims:          "2720",
 		Describe: `
 			受巴西大豆, 美大豆到港量影响, 美大豆价格不断近期新低
-			进口国外猪肉打压国内猪肉价格影响
+			进口国外猪肉打压国内猪肉价格影响养殖猪产生的利润
 			短期豆粕低位横盘，甚至会在破新低, 目标01合约, 2720
+			近期出现低点2750+, 开始反转的样子
+			五月合约交割价, 预估3000以上, 目前200+基点, 或有补基差行情
 		`,
-		Level:          2,
+		Level:          3,
 		PricePrecision: 0,
 		IsShow:         true,
 	},
-	"BU2012": &Variety{
+	"bu2012": &Variety{
 		Name:          "沥青",
 		OriginDataUrl: "113_bu2012_qt?callbackName=aa&cb=aa&_=1587532980754",
-		SpotPrice:     2250,
-		Aims:          "0",
+		SpotPrice:     2252.5,
+		Aims:          "",
 		Describe: `
-			
+			从一个足够长的周期来看, 价格会回归相对的内在价值与正常供需所在的价格
+			长期看涨, 但短期严重受原油行情的压制
+			不做单边观点, 与燃油做反套
+			多沥青空燃油, 只做日内, 预期一个交易日有一到两次机会产生20～30个点的基差
+			不跨日持仓, 日内平仓或者锁定仓位, 不赚跨日的钱, 防止大的风险, 只赚日内价差
 		`,
-		Level:          3,
+		Level:          1,
 		PricePrecision: 0,
 		IsShow:         true,
 	},
@@ -119,23 +125,69 @@ var varietys = map[string]*Variety{
 		Name:          "燃油",
 		OriginDataUrl: "113_fu2101_qt?callbackName=aa&cb=aa&_=1587533000837",
 		SpotPrice:     0,
-		Aims:          "0",
+		Aims:          "",
 		Describe: `
-			
+			航运逻辑受疫情影响较弱, 又受到原油行情的压制, 偏弱运行
+			不做单边观点, 与沥青联合做
+			逻辑 沥青的副逻辑基建 强于 燃油的副逻辑航运
 		`,
-		Level:          3,
+		Level:          1,
 		PricePrecision: 0,
 		IsShow:         true,
 	},
-	"AG2012": &Variety{
+	"ag2012": &Variety{
 		Name:          "白银",
 		OriginDataUrl: "113_ag2012_qt?callbackName=aa&cb=aa&_=1587533190507",
 		SpotPrice:     0,
-		Aims:          "0",
+		Aims:          "3380",
 		Describe: `
-			
+			技术面受压制, 3650 可空, 目标3380
+			但受黄金影响, 容易出黑天鹅, 观察为主, 不动
 		`,
-		Level:          3,
+		Level:          4,
+		PricePrecision: 0,
+		IsShow:         true,
+	},
+	"au2012": &Variety{
+		Name:          "黄金",
+		OriginDataUrl: "113_au2012_qt?callbackName=aa&cb=aa&_=1587566439644",
+		SpotPrice:     0,
+		Aims:          "",
+		Describe: `
+			围绕 360 上下 30 个点震荡, 跟着外盘跑
+			技术面上看在 380/390 受压制, 但
+			风险较大, 伦敦黄金可能存在逼仓的可能, 导致外盘黄金突破新高
+			进而引发内盘黄金突破平台区, 观察为主, 不动
+		`,
+		Level:          4,
+		PricePrecision: 2,
+		IsShow:         true,
+	},
+	"jd2010": &Variety{
+		Name:          "鸡蛋",
+		OriginDataUrl: "114_jd2010_qt?callbackName=aa&cb=aa&_=1587566503238",
+		SpotPrice:     3142,
+		Aims:          "",
+		Describe: `
+			看震荡行情
+			4100以下可多, 4300以上可空
+		`,
+		Level:          4,
+		PricePrecision: 0,
+		IsShow:         true,
+	},
+	"a2009": &Variety{
+		Name:          "豆一",
+		OriginDataUrl: "114_a2009_qt?callbackName=aa&cb=aa&_=1587566549527",
+		SpotPrice:     4666.67,
+		Aims:          "4800~5000",
+		Describe: `
+			妖的一逼, 现货价格强劲, 没有进行扩产, 交易所交割标准上升
+			如上原因构建一个强大的豆一多头行情, 并且大波动大回踩, 但上升趋势不改
+			不做, 纯粹就是验证想法, 为我近期最大的亏损, 把它盯完
+			其实吧, 很可能五月多头移仓九月, 继续逼仓
+		`,
+		Level:          4,
 		PricePrecision: 0,
 		IsShow:         true,
 	},
