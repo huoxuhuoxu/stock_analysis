044490294a7e9cf9be3001688d9e52674ee484b8
huoxuhuoxu <530607199@qq.com>
2020-04-23 21:16:02 +0800

    update

--- show/init.go
+++ show/init.go
@@ -54,19 +54,38 @@ type Variety struct {
 		7. 相信自己的判断, 坚信自己的决定, 坚定不移的执行定下的策略, 贯彻到底, 始终如一
 */
 var varietys = map[string]*Variety{
-	"sc2006": &Variety{
-		Name:          "原油",
-		OriginDataUrl: "142_sc2006_qt?callbackName=aa&cb=aa&_=1587309656220",
-		SpotPrice:     0,
-		Aims:          "",
-		Describe: `
-		`,
+	"cl00y": &Variety{
+		Name:           "美油06",
+		OriginDataUrl:  "102_CL00Y_qt?callbackName=aa&cb=aa&_=1587646825954",
+		SpotPrice:      0,
+		Aims:           "",
+		Describe:       ``,
 		Level:          1,
+		PricePrecision: 2,
+		IsShow:         true,
+	},
+	"cl20u": &Variety{
+		Name:           "美油09",
+		OriginDataUrl:  "102_CL20U_qt?callbackName=aa&cb=aa&_=1587646921066",
+		SpotPrice:      0,
+		Aims:           "",
+		Describe:       ``,
+		Level:          1,
+		PricePrecision: 2,
+		IsShow:         true,
+	},
+	"sc2006": &Variety{
+		Name:           "原油06",
+		OriginDataUrl:  "142_sc2006_qt?callbackName=aa&cb=aa&_=1587309656220",
+		SpotPrice:      0,
+		Aims:           "",
+		Describe:       ``,
+		Level:          2,
 		PricePrecision: 1,
 		IsShow:         true,
 	},
 	"sc2009": &Variety{
-		Name:          "原油",
+		Name:          "原油09",
 		OriginDataUrl: "142_sc2009_qt?callbackName=aa&cb=aa&_=1587309656220",
 		SpotPrice:     0,
 		Aims:          "250~270",
@@ -78,113 +97,25 @@ var varietys = map[string]*Variety{
 			从时间周期的角度上看, 实际原油的价格只会降低, 最终买方需要支付的其实是卖方仓储的费用,
 			暂看震荡下行
 		`,
-		Level:          1,
-		PricePrecision: 1,
-		IsShow:         true,
-	},
-	"sc2012": &Variety{
-		Name:          "原油",
-		OriginDataUrl: "142_sc2012_qt?callbackName=aa&cb=aa&_=1587309656220",
-		SpotPrice:     0,
-		Aims:          "",
-		Describe: `
-		`,
-		Level:          1,
-		PricePrecision: 1,
-		IsShow:         true,
-	},
-	"rb2101": &Variety{
-		Name:          "螺纹",
-		OriginDataUrl: "113_rb2101_qt?callbackName=aa&cb=aa&_=1587308954178",
-		SpotPrice:     3405.5,
-		Aims:          "3400",
-		Describe: `
-			供需强劲, 但价格上涨受制于高库存
-			关联原料铁矿石也很强劲, 进入平台区, 一旦突破, 3400指日可待
-			反之, 3100也很容易回踩, 3250为中线, 上下各150点宽幅震荡
-		`,
 		Level:          2,
-		PricePrecision: 0,
+		PricePrecision: 1,
 		IsShow:         true,
 	},
-	"i2009": &Variety{
-		Name:          "铁矿石",
-		OriginDataUrl: "114_i2009_qt?callbackName=aa&cb=aa&_=1587396948222",
-		SpotPrice:     663.89,
-		Aims:          "640",
-		Describe: `
-			减产, 但是四五月份到港量高压制价格继续上行
-			可以与螺纹联做, 日内 多铁矿空螺纹
-			目前 铁矿 强于 螺纹, 1:2 或 2:3 建仓, 不跨日持仓
-		`,
+	"gc20z": &Variety{
+		Name:           "黄金",
+		OriginDataUrl:  "101_GC20Z_qt?callbackName=aa&cb=aa&_=1587647068923",
+		SpotPrice:      0,
+		Aims:           "",
+		Describe:       ``,
 		Level:          3,
 		PricePrecision: 1,
 		IsShow:         true,
 	},
-	"m2101": &Variety{
-		Name:          "豆粕",
-		OriginDataUrl: "114_m2101_qt?callbackName=aa&cb=aa&_=1585752611719",
-		SpotPrice:     3116.25,
-		Aims:          "2720",
-		Describe: `
-			受巴西大豆, 美大豆到港量影响, 美大豆价格不断近期新低
-			进口国外猪肉打压国内猪肉价格影响养殖猪产生的利润
-			短期豆粕低位横盘，甚至会在破新低, 目标01合约, 2720
-			近期出现低点2750+, 开始反转的样子
-			五月合约交割价, 预估3000以上, 目前200+基点, 或有补基差行情
-		`,
-		Level:          2,
-		PricePrecision: 0,
-		IsShow:         true,
-	},
-	"bu2012": &Variety{
-		Name:          "沥青",
-		OriginDataUrl: "113_bu2012_qt?callbackName=aa&cb=aa&_=1587532980754",
-		SpotPrice:     2252.5,
-		Aims:          "",
-		Describe: `
-			从一个足够长的周期来看, 价格会回归相对的内在价值与正常供需所在的价格
-			长期看涨, 但短期严重受原油行情的压制
-			不做单边观点, 与燃油做反套
-			多沥青空燃油, 只做日内, 预期一个交易日有一到两次机会产生20～30个点的基差
-			不跨日持仓, 日内平仓或者锁定仓位, 不赚跨日的钱, 防止大的风险, 只赚日内价差
-		`,
-		Level:          2,
-		PricePrecision: 0,
-		IsShow:         true,
-	},
-	"fu2101": &Variety{
-		Name:          "燃油",
-		OriginDataUrl: "113_fu2101_qt?callbackName=aa&cb=aa&_=1587533000837",
-		SpotPrice:     0,
-		Aims:          "",
-		Describe: `
-			航运逻辑受疫情影响较弱, 又受到原油行情的压制, 偏弱运行
-			不做单边观点, 与沥青联合做
-			逻辑, 同为原油相关性极高的衍生品 沥青的副逻辑基建 强于 燃油的副逻辑航运
-		`,
-		Level:          2,
-		PricePrecision: 0,
-		IsShow:         true,
-	},
-	"ag2012": &Variety{
-		Name:          "白银",
-		OriginDataUrl: "113_ag2012_qt?callbackName=aa&cb=aa&_=1587533190507",
-		SpotPrice:     0,
-		Aims:          "3380",
-		Describe: `
-			技术面受压制, 3650 可空, 目标3380
-			但受黄金影响, 容易出黑天鹅, 观察为主, 不动
-		`,
-		Level:          4,
-		PricePrecision: 0,
-		IsShow:         true,
-	},
 	"au2012": &Variety{
-		Name:          "黄金",
+		Name:          "沪金",
 		OriginDataUrl: "113_au2012_qt?callbackName=aa&cb=aa&_=1587566439644",
 		SpotPrice:     0,
-		Aims:          "",
+		Aims:          "360~390",
 		Describe: `
 			围绕 360 上下 30 个点震荡, 跟着外盘跑
 			技术面上看在 380/390 受压制, 但
@@ -195,51 +126,23 @@ var varietys = map[string]*Variety{
 		PricePrecision: 2,
 		IsShow:         true,
 	},
-	"jd2009": &Variety{
-		Name:          "鸡蛋",
-		OriginDataUrl: "114_jd2009_qt?callbackName=aa&cb=aa&_=1587566503238",
-		SpotPrice:     3142,
-		Aims:          "4100~4300",
-		Describe: `
-			看震荡行情
-			4100以下可多, 4300以上可空
-		`,
-		Level:          3,
-		PricePrecision: 0,
-		IsShow:         true,
-	},
-	"a2009": &Variety{
-		Name:          "豆一",
-		OriginDataUrl: "114_a2009_qt?callbackName=aa&cb=aa&_=1587566549527",
-		SpotPrice:     4666.67,
-		Aims:          "4800~5000",
-		Describe: `
-			妖的一逼, 现货价格强劲, 没有进行扩产, 交易所交割标准上升
-			如上原因构建一个强大的豆一多头行情, 并且大波动大回踩, 但上升趋势不改
-			不做, 纯粹就是验证想法, 为我近期最大的亏损, 把它盯完
-			其实吧, 很可能五月多头移仓九月, 继续逼仓
-		`,
-		Level:          4,
-		PricePrecision: 0,
-		IsShow:         true,
-	},
 }
 
 /*
 	请严格执行
 */
 var dailyOperation = `
-	1. 在合适的价格增仓 燃油、沥青, 依旧按照 空燃油多沥青 做差价套利的逻辑, 视实际情况增仓到单边 4～5 手
-	2. 螺纹解除2手空单, 共计持4手多单, 看涨, 收盘前在相对合适的位置, 再次锁仓2手多单
-	3. 继续观察 豆粕, 鸡蛋 走势, 豆一 4800 可短空
-	4. 暂时不做 螺纹/铁矿 反套, 留保证金给 燃油/沥青 反套, 扩大仓位, 让利润奔跑
-	5. 如果盘面 黑色 强势的惊人, 可日内短多 铁矿石, 收盘前平仓
-`
+	外盘美油 近月反弹多, 沅月反弹少
+	国内原油 近月反弹少, 沅月反弹多, 主要原因在于:
+		1. 美油近月跌多, 远月跌少, 在基本面可能改善的情况下, 近月反弹空间更大
+		2. 内盘远近合约跌势相近, 并且在对未来有较好预期的情况下, 远月反弹预期高于近月
+		3. 基本美油这波价格战中, 远月价格中枢依旧维持在 30～35 之间
 
-/*
-	注意:
-		原油涨时燃油不弱, 此时与沥青的反套基本没有利润
-		视原油情况而定, 涨-正套, 跌-反套
-		并且就观察下来, 跨日意义不大, 除非开盘锁仓,
-		开盘时会有20～30利差, 盘中修复至5～10, 基于原油涨的基础
-*/
+
+	操作该要:
+		原油,明日可交易情况下,
+		不出意外明日开盘以涨为主, 如果平开或者09合约280以下开, 可短多
+
+		黄金,
+		盘中看趋势, 可短空, 目前处于压力位, 不管内外盘, 可以顺势空一波
+`
