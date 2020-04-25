248e2845aca822d0952c701b85ed3b3107b07da6
huoxuhuoxu <530607199@qq.com>
2020-04-25 17:40:42 +0800

    update

---
+++ basis/init.go
@@ -0,0 +1,123 @@
+package main
+
+var (
+	headers = map[string]string{
+		"Accept":                    " text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+		"Accept-Encoding":           "gzip, deflate",
+		"Accept-Language":           "zh-CN,zh;q=0.9,en;q=0.8",
+		"Cache-Control":             "no-cache",
+		"Cookie":                    "qgqp_b_id=175ececc76779338ed237e0147093167; st_si=48998687825010; st_asi=delete; emshistory=%5B%22bai%20y%22%5D; HAList=a-sz-000061-%u519C%20%u4EA7%20%u54C1; em_hq_fls=js; st_pvi=61646748513010; st_sp=2020-03-29%2023%3A01%3A34; st_inirUrl=http%3A%2F%2Ffutures.eastmoney.com%2Fqihuo%2FM.html; st_sn=215; st_psi=20200401231508322-113200301325-3052892464",
+		"Host":                      "futsse.eastmoney.com",
+		"Pragma":                    "no-cache",
+		"Proxy-Connection":          "keep-alive",
+		"Upgrade-Insecure-Requests": "1",
+		"User-Agent":                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+	}
+
+	/*
+		@desc
+			basis < limit: 开仓
+			basis > profit: 反向/平仓
+			other: 等待
+	*/
+	actions = []string{"等待", "开仓", "反向/平仓"}
+)
+
+const (
+	ORIGIN_URL = "http://futsse.eastmoney.com/static/"
+)
+
+// 合约
+type Contract struct {
+	code  string  // 商品编号
+	open  float64 // 开盘价
+	close float64 // 昨日收盘价
+	high  float64 // 盘中最高价
+	low   float64 // 盘中最低价
+	price float64 // 现价
+}
+
+type Variety struct {
+	Name            string  // 商品名称
+	OriginDataUrl   string  // 数据源头
+	Price           float64 // 当前价格
+	Value           float64 // 涨跌值
+	PricePrecision  uint8   // 价格精度
+	Amount          int     // 套利持仓数
+	Dash            float64 // 划点, 对价平仓需要付出的价钱
+	DashCoefficient float64 // 每个单位点的价钱
+}
+
+type Group struct {
+	Name              string    // 组合名称
+	Combination       [2]string // 包含合约, 持仓方向(0: 多, 1: 空)
+	Limit             float64   // 基差临界点数
+	Profit            float64   // 利润点数
+	MarginConsumption string    // 组合需要消耗保证金
+	Level             int       // 优先级
+	Describe          string    // 组合逻辑说明
+}
+
+// 反套组
+var groups = []Group{
+	Group{
+		Name:              "多沥青/空燃油",
+		Combination:       [2]string{"bu2012", "fu2101"},
+		Limit:             5,
+		MarginConsumption: "5k",
+		Level:             1,
+		Profit:            30,
+		Describe: `
+			在原油为主导因素情况下, 
+			利用多空对冲对与原油具有高度相关性的衍生品进行对赌,
+			赌 沥青副逻辑-基建 强于 燃油副逻辑-航运,
+			不论涨或者跌, 在盘中体现, 沥青强于燃油, 
+			日内会产生 20～40 点的套利机会
+		`,
+	},
+	Group{
+		Name:              "多黄金/空白银",
+		Combination:       [2]string{"au2012", "ag2012"},
+		Limit:             5,
+		MarginConsumption: "5w",
+		Level:             1,
+		Profit:            30,
+		Describe:          ``,
+	},
+}
+
+// 合约集合
+var varietys = map[string]*Variety{
+	"bu2012": &Variety{
+		Name:            "沥青",
+		OriginDataUrl:   "113_bu2012_qt?callbackName=aa&cb=aa&_=1587532980754",
+		PricePrecision:  0,
+		Amount:          1,
+		Dash:            2,
+		DashCoefficient: 10,
+	},
+	"fu2101": &Variety{
+		Name:            "燃油",
+		OriginDataUrl:   "113_fu2101_qt?callbackName=aa&cb=aa&_=1587533000837",
+		PricePrecision:  0,
+		Amount:          1,
+		Dash:            1,
+		DashCoefficient: 10,
+	},
+	"au2012": &Variety{
+		Name:            "沪金",
+		OriginDataUrl:   "113_au2012_qt?callbackName=aa&cb=aa&_=1587566439644",
+		PricePrecision:  2,
+		Amount:          1,
+		Dash:            0.02,
+		DashCoefficient: 1000,
+	},
+	"ag2012": &Variety{
+		Name:            "白银",
+		OriginDataUrl:   "113_ag2012_qt?callbackName=aa&cb=aa&_=1587533190507",
+		PricePrecision:  0,
+		Amount:          3,
+		Dash:            1,
+		DashCoefficient: 15,
+	},
+}
---
+++ basis/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+)
+
+var (
+	contractChan = make(chan *Contract, 1)
+	mode         int
+)
+
+func init() {
+	flag.IntVar(&mode, "mode", 1, "level相关优先级及输出")
+	flag.Parse()
+}
+
+func main() {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// 特殊优先级: 0, 输出
+	if mode == 0 {
+		fmt.Printf("暂无")
+		return
+	}
+
+	// 价格监控
+	for _, group := range groups {
+		if group.Level <= mode {
+			for _, k := range group.Combination {
+				v := varietys[k]
+				go getData(k, ORIGIN_URL+v.OriginDataUrl)
+			}
+		}
+	}
+	go show()
+
+	<-interrupt
+}
+
+// 输出显示
+func show() {
+	// 1s刷新输出
+	t := time.Tick(time.Second * 1)
+	for {
+		select {
+		case <-t:
+			cmd := exec.Command("clear")
+			cmd.Stdout = os.Stdout
+			cmd.Run()
+
+			// 输出
+			for _, group := range groups {
+				curA := varietys[group.Combination[0]]
+				curB := varietys[group.Combination[1]]
+				if curA.Price == 0 || curB.Price == 0 {
+					continue
+				}
+
+				// 组合名称
+				name := fmt.Sprintf("%s/%s", group.Combination[0], group.Combination[1])
+				// 每一组的持仓比
+				matching := fmt.Sprintf("%d:%d", curA.Amount, curB.Amount)
+				// 每一组的平仓划点需要付出的价格
+				expenditure := curA.Dash*curA.DashCoefficient + curB.Dash*curB.DashCoefficient
+
+				// 有效精度
+				ppA := fmt.Sprintf("%%.%df", curA.PricePrecision)
+				ppB := fmt.Sprintf("%%.%df", curB.PricePrecision)
+				pp := fmt.Sprintf("%s/%s", ppA, ppB)
+
+				// 基差=(A的涨幅*A的手数*A每手的数量 - B同理) / 10, 1点=10元, 规整
+				basis := (curA.Value*float64(curA.Amount)*curA.DashCoefficient - curB.Value*float64(curB.Amount)*curB.DashCoefficient) / 10
+
+				// 判断操作行为
+				aI := 0
+				if basis <= group.Limit {
+					aI = 1
+				}
+				if basis >= group.Profit {
+					aI = 2
+				}
+
+				// show
+				fmt.Printf("%s, %.0f, "+pp+", "+pp+", %s, %.0f, %s, %s\n",
+					name,
+					basis,
+					curA.Price, curB.Price,
+					curA.Value, curB.Value,
+					matching, expenditure, actions[aI], group.MarginConsumption)
+
+				fmt.Println("----")
+			}
+
+		case contract := <-contractChan:
+			if v, ok := varietys[contract.code]; ok {
+				v.Price = contract.price
+				v.Value = contract.price - contract.close
+			}
+		}
+	}
+}
+
+// 更新数据
+func getData(k, dataUrl string) {
+	t := time.Tick(time.Second * 2)
+	client := &http.Client{}
+
+	for {
+		select {
+		case <-t:
+			req, _ := http.NewRequest("GET", dataUrl, nil)
+			for hk, hv := range headers {
+				req.Header.Set(hk, hv)
+			}
+
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Println(err)
+				break
+			}
+
+			respData, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Println(err)
+				break
+			}
+			resp.Body.Close()
+
+			respStr := string(respData)
+			respStr = strings.Replace(respStr, "aa(", "", 1)
+			respStr = strings.Replace(respStr, "})", "}", 1)
+
+			var data = make(map[string]interface{})
+			json.Unmarshal([]byte(respStr), &data)
+
+			if _, ok := data["qt"]; ok {
+				qt := data["qt"].(map[string]interface{})
+				contract := &Contract{
+					code:  k,
+					open:  qt["o"].(float64),
+					close: qt["qrspj"].(float64),
+					high:  qt["h"].(float64),
+					low:   qt["l"].(float64),
+					price: qt["p"].(float64),
+				}
+
+				contractChan <- contract
+			}
+		}
+	}
+}
